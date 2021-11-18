409ad9f0f22677c6ea9ec477d604a4383527de00
quovanitish <nitish.sharma1@quovantis.com>
2021-11-18 16:44:43 +0530

    Added CORS package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRouter = require("./core/routers/user/user");
 const taskRouter = require("./core/routers/task/task");
+const cors = require("cors");
 require("dotenv").config();
 require("./core/db/db");
 
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(taskRouter);
 
